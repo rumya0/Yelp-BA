0a231b990e306b0176a4321d906bb9b8f62fb6b5
Ramya <48737377+rumya0@users.noreply.github.com>
2020-02-19 21:22:07 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,27 +1,30 @@
+#Loading dependencies
 import pandas as pd
 import numpy as np
 
+#Loading the json file and reading into pandas
 business_json_path = 'business.json'
 df_b = pd.read_json(business_json_path, lines=True)
 
+#Subsetting the YELP dataset to restaurant-only dataset
 business_restaur = df_b[df_b['categories'].str.contains('Restaurants', case=False, na=False)]
 
+#Filtering for best performing restaurants i.e restaurants with a 4.5 rating or more - TopTierRestaurant Dataset
 top_res = business_restaur [business_restaur['stars']>= 4.5]
 
+#Counting the number of restaurants in each city
 top_res_cities = top_res['city'].value_counts()
 
+#res_cat dataset contains restaurants with 4.5 or more ratings in cities that are similar to cambridge, MA
+#'Phoenix','Pittsburgh', 'Charlotte', 'Cleveland', 'Mesa', 'Tempe', 'Madison', 'Missisauga', 'Glendale'
 res_cat = top_res[(top_res['city'].isin(['Phoenix','Pittsburgh', 'Charlotte', 'Cleveland', 'Mesa', 'Tempe', 'Madison', 'Missisauga', 'Glendale']))]
-#res_cat dataset contains restaurants with more than 4 stars in towns that are similar to cambridge:
-#Toronto, Montreal, Phoenix, Pittsburgh, Cleveland, Tempe, Scottsdale, Charlotte
 
-#This gives you a list of the most frequent types of restaurants in 
+#Counting the frequency of each type of restaurant in the TopTier restaurant
 topres = pd.Series(','.join(res_cat.categories).split()).value_counts()
 
-#This code already dropped the keywords so if you run it again, it wont work
+#Removing the redundant descriptions in the 'categories'
 topres = topres.drop(['Restaurants,','&','Food,','Restaurants,Restaurants,','(New),','(Traditional),'])
 
-#All the restaurants in the rescat dataset are restaurants in the above mentioned cities
-#with rating of more than 4.5 stars
 
 
 
